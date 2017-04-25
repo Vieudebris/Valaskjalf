e25204922779c3292f1a580716d05c6d7295a6ae
Spinoza <salome.desauty@epita.fr>
2017-04-25 14:38:08 +0200

    Menu (things need to be fixed)

---
+++ Assets/Resources/Scripts/MenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+
+public class MenuScript : MonoBehaviour {
+
+    Button butt; 
+    public string newsc; 
+
+	void Start ()
+    {
+        butt = GetComponent<Button>();
+        butt.onClick.AddListener(LoadScene); 
+	}
+	public void LoadScene ()
+    {
+        SceneManager.LoadScene(newsc, LoadSceneMode.Single); 
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
